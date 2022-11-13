040c7d6f14f1bf904f84df14f6621eca4e0ad6aa
Kovács Tibor <ifjutibor@gmail.com>
2022-11-13 17:10:21 +0100

    feat(loader): add loader while we fetch the endpoint

--- snow-phoria/popup.js
+++ snow-phoria/popup.js
@@ -1,5 +1,5 @@
 async function fetchData() {
-  const apiKey = 'API_KEY';
+  const apiKey = '5a93b1b61cmshdabcc6e5056f1cfp13faf3jsn018b95b2dd7a';
   const options = {
     method: 'GET',
     headers: {
@@ -11,9 +11,10 @@ async function fetchData() {
  const response = await fetch('https://foreca-weather.p.rapidapi.com/observation/latest/103054643?lang=en', options)
  const record = await response.json();
 
- document.getElementById('concerts').innerHTML =
+  document.getElementById('loader').remove();
+  document.getElementById('concerts').innerHTML =
    record.observations
      .map(observation => `<li>${observation.station}: ${observation.temperature}°C</li>`)
-     .join('');
+     .join('')
 }
 fetchData();
